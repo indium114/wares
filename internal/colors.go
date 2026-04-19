a6860c50392d239dee10be908b605ad23d0a96b1
indium114 <156162907+indium114@users.noreply.github.com>
2026-04-19 15:46:22 +0200

    feat: change ERR to ERROR in ErrText

--- internal/colors.go
+++ internal/colors.go
@@ -7,7 +7,7 @@ import (
 var (
 	OkText     string = color.GreenString("[OK]")
 	WarnText   string = color.YellowString("[WARN]")
-	ErrText    string = color.RedString("[ERR]")
+	ErrText    string = color.RedString("[ERROR]")
 	SyncText   string = color.BlueString("[SYNC]")
 	UpdateText string = color.MagentaString("[UPDATE]")
 	DebugText  string = color.HiBlackString("[DEBUG]")
