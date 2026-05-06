a23c24b784374a9e12bab6df4c9e90948dab66d7
indium114 <156162907+indium114@users.noreply.github.com>
2026-05-06 18:23:27 +0200

    feat: ensureBlueprintRepo

---
+++ internal/blueprints.go
@@ -0,0 +1,41 @@
+package internal
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+func ensureBlueprintRepo(repo string) (string, error) {
+	home, _ := os.UserHomeDir()
+	base := filepath.Join(home, ".local", "share", "wares")
+
+	parts := strings.Split(repo, "/")
+	dir := filepath.Join(base, parts[0], parts[1])
+
+	// ensure that repo exists (like the function name :P)
+	if _, err := os.Stat(filepath.Join(dir, ".git")); err == nil {
+		// pull latest changes
+		fmt.Printf("%s Pulling %s\n", HintText, repo)
+		cmd := exec.Command("git", "-C", dir, "pull", "origin")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			return "", err
+		}
+		return dir, nil
+	}
+
+	// clone if it doesn't exist
+	os.MkdirAll(filepath.Dir(dir), 0o755)
+	fmt.Printf("%s Cloning %s\n", HintText, repo)
+	cmd := exec.Command("git", "clone", repo, dir)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return "", err
+	}
+	return dir, nil
+}
