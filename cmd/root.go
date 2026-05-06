c67de7cb8b1e5a601a6e77ce787f085e5392e8d5
indium114 <156162907+indium114@users.noreply.github.com>
2026-05-06 15:56:32 +0200

    fix: make --version flag actually work

--- cmd/root.go
+++ cmd/root.go
@@ -8,12 +8,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var version = "0.5.1"
+
 var rootCmd = &cobra.Command{
 	Use:   "wares",
 	Short: "A declarative AppImage/binary package manager",
 }
 
 func Execute() {
+	rootCmd.Version = version
+	rootCmd.SetVersionTemplate(`{{.Version}}`)
 	err := fang.Execute(context.Background(), rootCmd)
 	if err != nil {
 		os.Exit(1)
