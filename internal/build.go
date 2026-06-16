b870fb551deabd33c45d6c57d40dd8c3e661cdec
indium114 <156162907+indium114@users.noreply.github.com>
2026-06-16 12:28:57 +0200

    feat: build _self blueprint

---
+++ internal/build.go
@@ -0,0 +1,58 @@
+package internal
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/indium114/slag"
+)
+
+func buildStoreBase(absDir string) string {
+	home, _ := os.UserHomeDir()
+	rel := strings.TrimPrefix(absDir, "/")
+	return filepath.Join(home, ".local", "share", "wares", "_builds", rel)
+}
+
+func BuildFromWaresfile(dir string) error {
+	oldHome := os.Getenv("WARES_HOME")
+	os.Setenv("WARES_HOME", buildStoreBase(dir))
+	defer os.Setenv("WARES_HOME", oldHome)
+
+	shellConfig, err := LoadShellConfig(dir)
+	if err != nil {
+		return err
+	}
+
+	bp, ok := shellConfig.Blueprints["_self"]
+	if !ok {
+		return slag.Err("no '_self' blueprint in the specified directory's waresfile.yaml")
+	}
+
+	bp.Repo = dir
+
+	slag.Build("%s\n", bp.Repo)
+
+	commit, err := resolveLatestCommit(dir)
+	if err != nil {
+		return err
+	}
+
+	repoDir, err := ensureBlueprintRepo(bp.Repo)
+	if err != nil {
+		return err
+	}
+
+	if err := buildBlueprint(repoDir, commit, bp.Steps); err != nil {
+		return err
+	}
+
+	for _, artifact := range bp.Artifacts {
+		slag.Log("symlinking artifact %s from repoDir %s to result dir %s", artifact, repoDir, dir+"/wares-result") // DEBUG
+		if err := shellSymlinkBlueprint(artifact, repoDir, dir+"/wares-result"); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
